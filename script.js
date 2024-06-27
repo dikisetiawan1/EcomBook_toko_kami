c77f474e150b86fd57596608e247e7dbe1e224a9
Diki Setiawan <dikisetia101@gmail.com>
2024-06-28 01:23:26 +0700

    update

--- script.js
+++ script.js
@@ -1,11 +1,131 @@
-const mainContainer = document.querySelector("#cards");
-const cart =document.querySelector("#cart");
+document.addEventListener("DOMContentLoaded", () => {
+const mainContainer = document.getElementById("cards");
+const cart = document.querySelector("#cart");
+const searchInput = document.getElementById("searchInput");
 
 
-// membuat fungsi search buku
-function searchBook(val){
-  return val;
+// membuat fungsi consume API
+// function getApi(){
+//   fetch("books.json").then(response =>{
+//     return response.json();
+//   }).then(dataJson => {
+//     return dataJson.booksJson;
+//   }).catch(error =>{
+//     console.log(error);
+//   })
+// }
 
+
+const product = [
+      {
+          "title":"The Birth of Tragedy",
+          "price":"75000",
+          "penulis":"Nietzsche",
+          "penerbit":"IRCiSoD",
+          "isbn":"978-623-8108-65-7",
+          "halaman":"252",
+          "img":"./assets/img/The-Birth-of-Tragedy.jpeg",
+          "description":"Friedrich Nietzsche (1844–1900) dalam buku ini membeberkan asal-usul tragedi Yunani..",
+          "kategori" :"non-fiksi"
+      },
+      {
+          "title":"Nietzsche dan Islam",
+          "price":"75000",
+          "penulis":"Roy Jackson",
+          "penerbit":"IRCiSoD",
+          "isbn":"-",
+          "halaman":"412",
+          "img":"./assets/img/Nietzsche-Islam.jpeg",
+          "description":"Friedrich Nietzsche adalah filsuf masyhur yang kerap diidentikkan dengan ateisme.. ",
+          "kategori" :"non-fiksi"
+      },
+      {
+          "title":"Genealogi Moral",
+          "price":"75000",
+          "penulis":"Basabasi",
+          "penerbit":"-",
+          "isbn":"-",
+          "halaman":"220",
+          "img":"./assets/img/geneologi-moral.jpeg",
+          "description":"Pemikiranku tentang genealogi moral mempunyai bentuk singkat dan.. ",
+          "kategori" :"non-fiksi"
+      },
+      {
+          "title":"Janji Yang Teringkar",
+          "price":"76000",
+          "penulis":"Ken Budha Kusumandaru",
+          "penerbit":"Instrans Publishing",
+          "isbn":"-",
+          "halaman":"148",
+          "img":"./assets/img/janji-yang-teringkar.jpg",
+          "description":"Masyarakat sipil dan organisasi non-pemerintah (NGO) adalah.. ",
+          "kategori" :"non-fiksi"
+      },
+      {
+          "title":"Konsep Judicial Review",
+          "price":"80000",
+          "penulis":"Ali Marwan HSB",
+          "penerbit":"-",
+          "isbn":"978-602-6344-19-9",
+          "halaman":"200",
+          "img":"./assets/img/konsep-judicial-review.jpg",
+          "description":"Sebuah produk peraturan perundang-undangan seharusnya...",
+          "kategori" :"non-fiksi"
+      },
+      {
+          "title":"Etnobotani Mangrove",
+          "price":"62000",
+          "penulis":"Miri Pariyas Tutik Fitriya",
+          "penerbit":"-",
+          "isbn":"-",
+          "halaman":"114",
+          "img":"./assets/img/etnobotani-mangrove.jpg",
+          "description":"Manusia memiliki hubungan dekat dengan alam, khususnya...",
+          "kategori" :"non-fiksi"
+      },
+  ]
+
+  const categories =  [...new Set(product.map((item)=>{
+    return item
+  }))]
+  
+  // membuat display card
+  function displayData(data){
+  const itemData = data.booksJson.map((item)=>{
+    return `
+    <div class="col-lg-4" data-aos="zoom-in" data-aos-duration="1000"
+       data-aos-delay="300"> 
+      <div class="card shadow card-featured">
+        <figure class="img-wrapper ">
+          <img src=${item.img} alt="" class="img-cover ">
+        </figure> 
+       <div class="card-body">
+        <h5 class="card-title">
+            ${item.title}
+        </h5>
+        <div class="card-text">
+          ${item.description}
+        </div>
+      </div>
+      <span >${rupiah(item.price)},-</span>
+       <hr/>
+       <div class="actions">
+        <div class="container mb-4">
+        <div class="row">
+          <div class="col-12">
+            <a href="#" class="btn btn-warning" type="button">Book detail</a>
+            <a href="#" class="btn btn-outline-warning" type="button" data-toggle="tooltip" data-placement="right" title="Add to cart">
+              <img  src="./assets/img/icons/cart-shopping-solid-black.svg" style="width: 20px;" alt="icons">
+              </a>
+          </div>
+        </div>     
+      </div>  
+       </div>
+      </div>
+    </div>
+    `
+}).join(" ");
+mainContainer.innerHTML=itemData;
 }
 
 // membuat modal cart
@@ -31,50 +151,4 @@ function rupiah(value) {
 }).format(value);
 return rupiahFormat;
 }
-
-// membuat fungsi consume API
-function getApi(){
-  const res = fetch("books.json").then(res =>{
-    return res.json();
-  }).then(data => {
-    const getData = data.booksJson.map((data)=>{
-        return `
-        <div class="col-lg-4" data-aos="zoom-in" data-aos-duration="1000"
-           data-aos-delay="300"> 
-          <div class="card shadow card-featured">
-            <figure class="img-wrapper ">
-              <img src=${data.img} alt="" class="img-cover ">
-            </figure> 
-           <div class="card-body">
-            <h5 class="card-title">
-                ${data.name}
-            </h5>
-            <div class="card-text">
-              ${data.description}
-            </div>
-          </div>
-          <span >${rupiah(data.price)},-</span>
-           <hr/>
-           <div class="actions">
-            <div class="container mb-4">
-            <div class="row">
-              <div class="col-12">
-                <a href="#" class="btn btn-warning" type="button">Book detail</a>
-                <a href="#" class="btn btn-outline-warning" type="button" data-toggle="tooltip" data-placement="right" title="Add to cart">
-                  <img  src="./assets/img/icons/cart-shopping-solid-black.svg" style="width: 20px;" alt="icons">
-                  </a>
-              </div>
-            </div>     
-          </div>  
-           </div>
-          </div>
-        </div>
-        `
-    }).join(" ");
-    mainContainer.innerHTML=getData;
-
-  }).catch(error =>{
-      console.log(error);
-  })
-} 
-getApi();
+});
