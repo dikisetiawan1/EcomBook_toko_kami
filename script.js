c614d4f81e5447b7b89e18a7a8ade6f16c2fc83d
Diki Setiawan <dikisetia101@gmail.com>
2024-07-02 23:32:35 +0700

    add fitur books detail

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const category2 = document.getElementById("category2");
 const allCategory = document.getElementById("allCategory");
 
 
+
+
+
 fetch("books.json").then(response =>{
     return response.json();
   }).then(dataJson => {
@@ -45,7 +48,6 @@ allCategory.addEventListener('input', (e)=>{
   displayItem(dataSelect);
   
 })
-displayItem(categories);
 
 // category2
 category2.addEventListener('input', (e)=>{
@@ -54,16 +56,10 @@ category2.addEventListener('input', (e)=>{
   if(data.category == searchCategory){
     const filterCategory= data.category;
     return filterCategory;
-
   }
-  
 }))
-// console.log(dataSelect)
 displayItem(dataSelect);
-
 })
-displayItem(categories);
-  
 
 searchInput.addEventListener('keyup',(e) =>{
   const searchData = e.target.value.toLowerCase();
@@ -78,10 +74,11 @@ searchInput.addEventListener('keyup',(e) =>{
         displayItem(filterData)
       }
 })
+
 displayItem(categories);
 }
-  
   function displayItem(items){
+
   mainContainer.innerHTML = items.map((item)=>{
     return `
     <div class="col-lg-4 mt-5" data-aos="zoom-in" data-aos-duration="1000"
@@ -98,26 +95,100 @@ displayItem(categories);
           ${item.description}
         </div>
       </div>
-      <span >${rupiah(item.price)},-</span>
+      <span >${rupiah(item.price)}</span>
       <span hidden>${item.category}</span>
        <hr/>
        <div class="actions">
         <div class="container mb-4">
         <div class="row">
           <div class="col-12">
-            <a href="#" class="btn btn-warning mr-2" type="button">Book detail</a>
-            <a href="#" class="btn btn-outline-warning" type="button" data-toggle="tooltip" data-placement="right" title="Add to cart">
+          <a class="btn btn-warning mr-2" type="button" data-toggle="modal" data-target="#modal${item.id}" >Book detail</a>
+            <button class="btn btn-outline-warning" type="button" data-toggle="tooltip" data-placement="right" title="Add to cart" >
               <img  src="./assets/img/icons/cart-shopping-solid-black.svg" style="width: 20px;" alt="icons">
-              </a>
+              </button>
           </div>
         </div>     
       </div>  
        </div>
       </div>
     </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="modal${item.id}" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Book Detail</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+            <li class="list-group-item ">
+            <table class="table table-borderless">
+                <tbody>
+                  <tr>
+                    <th scope="row">Title</th>
+                    <td>:</td>
+                    <td>${item.title}</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Writer</th>
+                    <td>:</td>
+                    <td>${item.penulis}</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Publisher</th>
+                    <td>:</td>
+                    <td>${item.penerbit}</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Pages</th>
+                    <td>:</td>
+                    <td>${item.halaman}</td>
+                    <td></td>
+                  </tr><tr>
+                    <th scope="row">Category</th>
+                    <td>:</td>
+                    <td>${item.category}</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">ISBN</th>
+                    <td>:</td>
+                    <td>${item.isbn}</td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Description</th>
+                    <td>:</td>
+                    <td>${item.description}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Price</th>
+                    <td>:</td>
+                    <td>${rupiah(item.price)}</td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
+          
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-outline-warning" type="button" data-toggle="tooltip" data-placement="right" title="Add to cart">
+              <img  src="./assets/img/icons/cart-shopping-solid-black.svg" style="width: 20px;" alt="icons">
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     `
 }).join(" ");
 };
+
 
 document.addEventListener("click", cartShow);
 function cartShow() {
@@ -132,7 +203,6 @@ function cartShow() {
 </div>`
 }
 cart.innerHTML = cartShow();
-
 
 function rupiah(value) {
   let rupiahFormat = new Intl.NumberFormat('id-ID', {
